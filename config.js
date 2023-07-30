ee479c43bfcce57e54a221d4f93538eaf36ff1db
Farhannnnn <74123134+Fxc7@users.noreply.github.com>
2023-07-30 13:58:53 +0700

    and last.
    config function

--- config.js
+++ config.js
@@ -1,4 +1,24 @@
-export default {
+const configs = {
     title: 'xcoders - youtube downloader',
     apikey: 'YOUR APIKEY'
+};
+
+const formatSize = (bytes) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return '0 Byte';
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round((bytes / Math.pow(1024, i)) * 100) / 100 + ' ' + sizes[i];
+};
+
+const calculateDownloadSpeed = (startTime, receivedSize) => {
+    const endTime = Date.now();
+    const elapsedTimeInMs = endTime - startTime;
+    const downloadSpeedInBytesPerSec = receivedSize / (elapsedTimeInMs / 1000);
+    return downloadSpeedInBytesPerSec;
+}
+
+export default {
+    configs,
+    formatSize,
+    calculateDownloadSpeed
 }
